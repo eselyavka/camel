fc436054b542bda147113ff691737ea508676080
Evgenii Seliavka <eseliavka@griddynamics.com>
2015-12-20 20:00:05 +0300

    Main scripts

---
+++ camel_parser.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import logging
+import psycopg2
+import StringIO
+
+COUNTERS = { 'bad_record':0, 'good_record':0,
+             'normalized_numbers':0, 'no_need_normalization':0,
+             'bad_numbers':0, 'bad_release_code': 0
+           }
+
+logging.basicConfig(
+    format='\t'.join([
+        '%(asctime)s',
+        '%(name)s',
+        '%(levelname)s',
+        '(%(process)d)',
+        '%(message)s'
+    ]),
+    level=logging.ERROR
+)
+
+def extract_release_code(release_code_bytes):
+    raw_bytes = release_code_bytes.split(':')
+    if raw_bytes[1]:
+        return int(bin(int(raw_bytes[1], 16)), 2) & int('0b01111111', 2)
+    else:
+        COUNTERS['bad_release_code']
+        LOG.debug("Can't extract release code from %s", release_code_bytes)
+    return 0
+
+def normalize_number(msisdn):
+    if len(msisdn) == 11:
+        if msisdn.startswith('8', 0, 1):
+            LOG.debug("Perform normalization %s->%s", msisdn, '7' + msisdn[1:])
+            msisdn='7' + msisdn[1:]
+            COUNTERS['normalized_numbers'] += 1
+        else:
+            LOG.debug("No need normalization for %s", msisdn)
+            COUNTERS['no_need_normalization'] += 1
+    elif len(msisdn) == 10:
+        msisdn = '7' + msisdn
+    else:
+        LOG.debug("Bad number found: %s", msisdn)
+        COUNTERS['bad_numbers'] += 1
+    return msisdn
+
+def read_text_dump(filename):
+    global COUNTERS
+
+    with open(filename, 'r') as fh:
+        for line in fh:
+            arr = line.split(',')
+            if len(arr) == 6:
+                if arr[2]:
+                    normalized_numbers=list(map(normalize_number, [arr[1], arr[2]]))
+                    COUNTERS['good_record'] += 1
+                    yield ','.join([str(int(arr[0] * 1000)), ','.join(normalized_numbers), ','.join(arr[3:len(arr)-1]), str(extract_release_code(arr[len(arr)-1]))])
+                else:
+                    COUNTERS['bad_record'] += 1
+            else:
+                COUNTERS['bad_record'] += 1
+
+def database_upload(mem_file):
+    conn = psycopg2.connect("dbname=camel user=camel host=localhost password=camel")
+    cur = conn.cursor()
+    cur.copy_from(mem_file, 'camel_data', sep=',')
+    #cur.execute("select * from camel_data;")
+    #cur.fetchall()
+    cur.close()
+    conn.close()
+
+def main():
+    try:
+        mem_file = StringIO.StringIO()
+        for record in read_text_dump(sys.argv[1]):
+            mem_file.write(record + "\n")
+        mem_file.seek(0)
+        database_upload(mem_file)
+        mem_file.close()
+        #print COUNTERS
+    except IndexError:
+        LOG.error("Please specify file to read")
+
+if __name__=='__main__':
+    LOG = logging.getLogger(os.path.basename(sys.argv[0]))
+    main()
---
+++ camel_parser.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+CAMEL_PATH=''
+TSHARK_FILTER="(camel.eventTypeBCSM == 2 and camel.eventTypeBCSM == 17) or \
+               (camel.eventTypeBCSM == 2 and camel.eventTypeBCSM == 9) and \
+               camel.local == 44"
+OUTPUT_PATH=''
+
+# print help, error string and exit
+# Globals:
+#   None
+# Arguments:
+#   $1 - error string
+# Returns:
+#   None
+function print_help() {
+    [[ -n "${1}" ]] && echo -ne "${1}\n"
+    echo -ne 'Usage is:\n'
+    echo -ne '--camel-path= path to CAMEL pcap raw log files\n\n'
+    echo -ne '--output-path= path where to write output CSV files, default tmp\n\n'
+    exit 1
+}
+
+# parse CLI arguments
+# Globals:
+#   CAMEL_PATH
+# Arguments:
+#   $@ - CLI arguments
+# Returns:
+#   None
+function parse_command_line_arg() {
+    while [ $# -gt 0 ]
+    do
+        case $1 in
+            --help )
+            print_help
+            ;;
+
+            --camel-path=*)
+            param=$( echo "${1}" | sed -e 's/^[^=]*=//g' )
+            [[ "X" = "X${param}" ]] && print_help "invalid camel-path!"
+            CAMEL_PATH="${param}"
+            ;;
+
+            --output-path=*)
+            param=$( echo "${1}" | sed -e 's/^[^=]*=//g' )
+            [[ "X" = "X${param}" ]] && echo "output-path is not set,
+                                             using default /tmp!"
+            OUTPUT_PATH="${param:-/tmp}"
+            ;;
+
+            *)
+            # no more options, get on with life
+            print_help "Unknown parameter ${1}. Exiting."
+            ;;
+
+        esac
+        shift         # otherwise if no-match not shifted, would cause infinite loop
+    done
+}
+
+# parse CAMEL pcap dump files
+# Globals:
+#   CAMEL_PATH
+#   OUTPUT_PATH
+# Arguments:
+#   None
+# Returns:
+#   None
+function parse_camel() {
+    local file
+    local file_bn
+    local camel_raw_files=$(ls ${CAMEL_PATH}/*.pcap)
+    [[ -z "${camel_raw_files}" ]] && { echo "WARNING: There is no pcap \
+                                       files under ${CAMEL_PATH}"; exit 3;
+                                     }
+    for file in ${camel_raw_files} ; do
+        file_bn=$(basename "${file}")
+        tshark -r "${file}" -Y "${TSHARK_FILTER}" -n -T fields -E separator=, -e frame.time_epoch \
+                                                                              -e e164.calling_party_number.digits \
+                                                                              -e e164.called_party_number.digits \
+                                                                              -e camel.callReferenceNumber \
+                                                                              -e camel.callAttemptElapsedTimeValue \
+                                                                              -e camel.releaseCauseValue > "${OUTPUT_PATH}/${file_bn}.txt"
+    done
+}
+
+parse_command_line_arg "$@"
+
+[[ -z "${CAMEL_PATH}" ]] && print_help "camel-path is not set"
+
+parse_camel
